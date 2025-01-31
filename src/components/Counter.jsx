06981437a0cb20e99af8db338f3df4dea5fd9c62
Miriam <miriam@miriam.com>
2025-01-31 20:47:53 +0000

    astro primer commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Counter() {
+  return (
+    <div>
+      <h1>Contadores que se actualizan separadamente</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Hiciste clic {count} veces</button>;
+}
